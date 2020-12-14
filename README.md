1c189dbec35efe3a86faf8d7eae63f5eeb70e97d
Hiroshani Ekanayaka <hiroshanie@hotmail.com>
2020-12-14 14:32:40 +0530

    Login form automated.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # demo-qa-assignment
-Assignment project for demo qa website
+Assignment project for Demo QA website
+
+##Steps
+1. Clone the demo-qa-Assignment to local folder.
+2. Open the demo-qa-assignment project Using Interlij.
+3. Navigate to the src->test->resources path and open the config.properties file.
+4. Change the chrome.path value according to the local path which locate to the chromedriver.exe.
+5. Navigate to the src->test->java->com.assignment.demoqaassignment path and open the DemoQATest.java file.
+6. Right Click and run DemoQATest.java file.
+
+Note: RECAPTHA can not handle in selenium. Because CAPTCHA used for verify the human.
+
--- src/test/java/com/assignment/demoqaassignment/DemoQATest.java
+++ src/test/java/com/assignment/demoqaassignment/DemoQATest.java
@@ -1,9 +1,11 @@
 package com.assignment.demoqaassignment;
 
-
 import com.assignment.demoqaassignment.util.PropertyFileReader;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class DemoQATest {
@@ -13,10 +15,32 @@ public class DemoQATest {
     String url=prop.getProperty("config","url");
 
     @Test
-    public void testLoginSuccess(){
+    public void testLoginSuccess() throws Exception {
         System.setProperty("webdriver.chrome.driver",chromePath );
         WebDriver driver = new ChromeDriver();
         driver.get(url);
         driver.manage().window().maximize();
+        driver.findElement(By.xpath("//body/div[@id='app']/div[1]/div[1]/div[2]/div[1]/div[6]")).click();
+        Thread.sleep(3000);
+
+        driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/input[1]")).sendKeys("Git");
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//body/div[@id='app']/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/span[1]/span[1]/*[1]")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//button[@id='login']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//button[@id='newUser']")).click();
+        Thread.sleep(2000);
+
+        driver.findElement(By.xpath("//input[@id='firstname']")).sendKeys("Hiro2");
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//input[@id='lastname']")).sendKeys("Ekanayaka");
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//input[@id='userName']")).sendKeys("Hiro2");
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Hiro21@");
+        Thread.sleep(2000);
+
+        driver.findElement(By.xpath("//button[@id='register']")).click();
     }
 }
